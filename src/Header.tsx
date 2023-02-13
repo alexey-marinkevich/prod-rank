d71c5335abeac752efc48e6e7e0796039a708f6c
Alexey Marinkevich <24780101+alexey-marinkevich@users.noreply.github.com>
2023-02-13 22:01:23 +0100

    add breakpoints header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,22 @@
 function Header() {
   return (
-    <nav className="grid h-14 grid-cols-2 place-content-center p-3">
-      <div>
+    <nav
+      className="
+    grid h-14 grid-cols-2 place-content-center p-3
+    lg:grid-cols-3
+    "
+    >
+      <div className="hidden lg:block"></div>
+      <div className="lg:justify-self-center">
         <a href="#" className="p-2">
           Logo
         </a>
       </div>
       <div className="justify-self-end ">
-        <a href="#" className="justify-self-end rounded-lg border p-3">
+        <a
+          href="#"
+          className=" rounded-lg border-2 p-3 transition-colors hover:border-black hover:bg-black hover:text-white"
+        >
           Suggest Product
         </a>
       </div>
