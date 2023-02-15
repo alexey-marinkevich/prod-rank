39f94801914a98fc74104282d8bdbb1407d395f9
Alexey Marinkevich <24780101+alexey-marinkevich@users.noreply.github.com>
2023-02-15 21:04:09 +0100

    enhance project structure

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Header from '../Header';
+import ProductsSection from '../ProductsSection';
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <ProductsSection />
+    </>
+  );
+}
+
+export default HomePage;
