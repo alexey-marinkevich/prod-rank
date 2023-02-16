2c56e8726cab5c07d1169f62236825edb6ec5d2d
Alexey Marinkevich <24780101+alexey-marinkevich@users.noreply.github.com>
2023-02-16 19:47:50 +0100

    rearrange product page grid system

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -13,10 +13,13 @@ function ProductPage() {
             Go back
           </Link>
         </header>
-        <article className="col-start-1 row-start-1 lg:row-start-2 lg:grid lg:grid-cols-[7fr,1fr]">
+        <h1 className="text-writing-mode hidden max-h-[920px] text-6xl font-semibold lg:block">
+          Atom Shoes
+        </h1>
+        <article className="col-start-1 row-start-1 lg:row-start-2">
           <figure
             className='flex h-screen flex-col-reverse items-center bg-[url("https://cdn.shopify.com/s/files/1/0231/2060/9358/files/Home_BW_Closeup_1024x.jpg?v=1556563118")] bg-cover bg-center
-            lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-4 lg:h-[80vh] lg:items-start
+            lg:h-[80vh] lg:items-start
           '
           >
             {/* icon */}
@@ -28,13 +31,10 @@ function ProductPage() {
               Explore Site
             </a>
           </figure>
-          <h1
-            className="lg:text-writing-mode p-5 px-10 text-center text-4xl
-              font-semibold lg:col-start-3 lg:col-end-4 lg:row-start-1 lg:row-end-5 lg:max-h-[920px] lg:text-6xl"
-          >
+          <h1 className="p-5 px-10 text-center text-4xl font-semibold lg:hidden">
             Atoms Shoes
           </h1>
-          <p className="m-auto max-w-2xl p-2 pb-8 text-lg lg:col-start-1 lg:col-end-4 lg:row-start-4 lg:row-end-5 lg:p-10">
+          <p className="m-auto max-w-2xl p-2 pb-8 text-lg lg:p-10">
             Atoms may look simple, but they’re packed with features. Our elastic laces
             mean you only have to tie your shoes once. We developed a custom foam midsole
             that molds to your feet, making them even more comfortable with every wear.
