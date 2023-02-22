648cbe9ad8d8c1da100644f0e43b00fe9fb5e4a4
Alexey Marinkevich <24780101+alexey-marinkevich@users.noreply.github.com>
2023-02-22 21:08:03 +0100

    refactor product section

--- src/ProductsSection.tsx
+++ src/ProductsSection.tsx
@@ -1,18 +1,21 @@
+import { useLoaderData } from 'react-router-dom';
+import { Product } from './pages/ProductPage';
 import ProductCard from './ProductCard';
+import supabase from './supabase';
+
+export async function getProducts() {
+  const { data, error } = await supabase.from('products').select('*');
+  if (error) throw new Error('Could not fetch the products');
+  return data;
+}
 
 function ProductsSection() {
+  const products = useLoaderData() as Awaited<ReturnType<typeof getProducts>>; // router has an issue here, temporary solution;
   return (
     <section className="grid gap-5 px-2 py-7 md:grid-cols-2 lg:grid-cols-1">
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      {products.map((product: Product) => {
+        return <ProductCard key={product.id} props={product} />;
+      })}
     </section>
   );
 }
