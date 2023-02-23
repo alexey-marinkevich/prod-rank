4d21c1baab6c5f4817328b4798e7d2fa0be9b765
Alexey Marinkevich <24780101+alexey-marinkevich@users.noreply.github.com>
2023-02-23 22:20:37 +0100

    refactor product card to real data fetching

--- src/ProductCard.tsx
+++ src/ProductCard.tsx
@@ -1,7 +1,16 @@
-function ProductCard() {
+/* eslint-disable react/prop-types */
+//todo: fix props
+import { Link } from 'react-router-dom';
+import { Product } from './pages/ProductPage';
+
+function ProductCard({ props }: { props: Product }) {
+  const BASE_ROUTE =
+    'https://qfwsyrybrxidfdqfjkui.supabase.co/storage/v1/object/public/product-images/';
+
+  const { id, headImage, articleContent, productName } = props;
   return (
-    <a
-      href="#"
+    <Link
+      to={`/product/${id}`}
       className="
         group flex h-[600px] flex-col overflow-hidden rounded-3xl shadow-md transition-[transform,box-shadow] duration-500
         hover:scale-95 hover:shadow-xl lg:h-[400px] lg:flex-row
@@ -14,21 +23,15 @@ function ProductCard() {
         "
       >
         <div
-          className="
-            h-full w-full bg-[url('https://cdn.shopify.com/s/files/1/0231/2060/9358/files/Home_Gray_Loft_600x.jpg?v=1557771826')]
-            bg-cover transition-transform duration-500 group-hover:scale-105
-          "
+          className={`h-full w-full bg-cover transition-transform duration-500 group-hover:scale-105`}
+          style={{ backgroundImage: `url(${BASE_ROUTE + headImage})` }}
         />
       </div>
       <div className="h-1/3 overflow-hidden p-3 pb-6 lg:h-auto lg:w-1/2">
-        <h2 className="mb-1 text-2xl">Atom shoes</h2>
-        <p className="h-4/5 overflow-hidden text-base lg:h-[93%]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur tempora fuga
-          autem! Iusto repellendus natus nostrum suscipit modi odio quae veniam
-          praesentium culpa quia necessitatibus ducimus totam, hic, laboriosam vel.
-        </p>
+        <h2 className="mb-1 text-2xl">{productName}</h2>
+        <p className="h-4/5 overflow-hidden text-base lg:h-[93%]">{articleContent}</p>
       </div>
-    </a>
+    </Link>
   );
 }
 
