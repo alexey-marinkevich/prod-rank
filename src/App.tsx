d1886f3c55c16d664a1a133513b11d2029866cef
Alexey Marinkevich <24780101+alexey-marinkevich@users.noreply.github.com>
2023-02-24 19:33:32 +0100

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import { HomePage, ErrorPage, SuggestProductPage, ProductPage } from './pages';
+import { getProduct } from './pages/ProductPage';
+import { suggestProductAction } from './pages/SuggestProductPage';
+import ProductsSection, { getProducts } from './ProductsSection';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/suggest',
-    element: <SuggestProductPage />,
-  },
-  {
-    path: '/product/:id',
-    element: <ProductPage />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route errorElement={<ErrorPage />}>
+      <Route path="/" element={<HomePage />}>
+        <Route path="/:page" element={<ProductsSection />} loader={getProducts} />
+      </Route>
+      <Route
+        path="/suggest"
+        element={<SuggestProductPage />}
+        action={suggestProductAction}
+      />
+      <Route path="/product/:id" element={<ProductPage />} loader={getProduct} />
+    </Route>
+  )
+);
 
 function App() {
   return <RouterProvider router={router} />;
