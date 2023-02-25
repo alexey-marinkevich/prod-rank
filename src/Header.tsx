dada1e871fbe55b05219df4bcae46d4c47caf348
Alexey Marinkevich <24780101+alexey-marinkevich@users.noreply.github.com>
2023-02-25 23:12:40 +0100

    update header

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,20 +3,20 @@ function Header() {
   return (
     <section
       className="
-    grid h-14 grid-cols-[1fr,2fr] place-content-center p-2
-    lg:grid-cols-3
+    m-auto grid h-14 max-w-7xl grid-cols-[1fr,2fr]
+    place-content-center p-2 py-10 lg:grid-cols-3
     "
     >
       <div className="hidden lg:block"></div>
       <div className="lg:justify-self-center">
-        <Link to="/" className="p-2">
+        <Link to="/" className="p-4 pl-0 lg:pl-4">
           Logo
         </Link>
       </div>
       <div className="justify-self-end ">
         <Link
           to="/suggest"
-          className="rounded-lg border-2 p-3 transition-colors hover:border-black hover:bg-black hover:text-white"
+          className="rounded-lg border-2 p-3 transition-all hover:border-black hover:bg-black hover:text-white"
         >
           Suggest Product
         </Link>
