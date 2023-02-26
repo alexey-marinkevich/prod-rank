2ade4351dd7de1a1baebf777e71c0e7e7f9b0d0c
Alexey Marinkevich <24780101+alexey-marinkevich@users.noreply.github.com>
2023-02-26 23:16:08 +0100

    add pagination to products

--- src/ProductsSection.tsx
+++ src/ProductsSection.tsx
@@ -1,22 +1,70 @@
-import { useLoaderData } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
+import { Params, useLoaderData, useNavigate } from 'react-router-dom';
 import { Product } from './pages/ProductPage';
 import ProductCard from './ProductCard';
 import supabase from './supabase';
 
-export async function getProducts() {
-  const { data, error } = await supabase.from('products').select('*');
+const perPage = 10;
+
+export async function getProducts({ params }: { params: Params }) {
+  const page = Number(params.page);
+  const fromItem = page * perPage;
+  const toItem = fromItem + perPage - 1;
+
+  const { data, error, count } = await supabase
+    .from('products')
+    .select('*', { count: 'exact' })
+    .order('created_at', { ascending: false })
+    .eq('reviewed', false) // todo: change to true
+    .range(fromItem, toItem);
   if (error) throw new Error('Could not fetch the products');
-  return data;
+
+  return {
+    products: data,
+    count,
+    page,
+  };
 }
 
 function ProductsSection() {
-  const products = useLoaderData() as Awaited<ReturnType<typeof getProducts>>; // router has an issue here, temporary solution;
+  const { products, count, page } = useLoaderData() as Awaited<
+    ReturnType<typeof getProducts>
+  >; // router has an issue here, temporary solution;
+  const navigate = useNavigate();
+
+  const pagesCount: number = Math.ceil(Number(count) / perPage);
+
+  function handleChangePage({ selected }: { selected: number }) {
+    navigate(`/${selected}`);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
   return (
-    <section className="grid gap-5 px-2 py-7 md:grid-cols-2 lg:grid-cols-1">
-      {products.map((product: Product) => {
-        return <ProductCard key={product.id} props={product} />;
-      })}
-    </section>
+    <>
+      <section className="m-auto grid max-w-7xl gap-5 px-2 py-7 md:grid-cols-2 lg:grid-cols-1">
+        {products.map((product: Product) => {
+          return <ProductCard key={product.id} props={product} />;
+        })}
+      </section>
+      <ReactPaginate
+        pageCount={pagesCount}
+        initialPage={page}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={0}
+        onPageChange={handleChangePage}
+        containerClassName="m-auto grid gap-6 justify-items-center justify-center items-center p-10 sm:grid-flow-col sm:gap-1"
+        pageLinkClassName="rounded-lg bg-transparent py-3 px-5 text-lg text-neutral-600 transition-all duration-300 hover:bg-neutral-100"
+        activeLinkClassName="pointer-events-none rounded-lg bg-neutral-900 py-3 px-5 text-lg font-medium text-neutral-100 transition-all duration-300"
+        previousLinkClassName="rounded-lg bg-transparent py-3 px-5 text-lg text-neutral-900 transition-all duration-300 hover:bg-neutral-100"
+        nextLinkClassName="rounded-lg bg-transparent py-3 px-5 text-lg text-neutral-900 transition-all duration-300 hover:bg-neutral-100"
+        disabledClassName="pointer-events-none rounded-lg bg-transparent py-3 px-5 text-lg transition-all duration-300"
+        disabledLinkClassName="text-neutral-300"
+        breakClassName="hidden"
+      />
+    </>
   );
 }
 
