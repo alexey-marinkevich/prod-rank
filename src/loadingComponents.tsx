d49d045a87b025a2a9f3d7efafa7fd6616f1ee96
Alexey Marinkevich <24780101+alexey-marinkevich@users.noreply.github.com>
2023-03-06 16:37:39 +0100

    create loading component

---
+++ src/loadingComponents.tsx
@@ -0,0 +1,10 @@
+import { TbLoader2 } from 'react-icons/tb';
+
+export function PageLoader() {
+  return (
+    <div className="flex h-[70vh] flex-col items-center justify-center gap-1">
+      <TbLoader2 className="animate-spin text-6xl" />
+      <p className="animate-pulse">Loading...</p>
+    </div>
+  );
+}
