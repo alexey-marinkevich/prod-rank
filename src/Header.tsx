f6a7d23b04f48d28e24c6dbacea605b28a032cc8
Alexey Marinkevich <24780101+alexey-marinkevich@users.noreply.github.com>
2023-03-06 15:44:38 +0100

    update header with new icons

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,26 +1,33 @@
 import { Link } from 'react-router-dom';
+import { SiStarship } from 'react-icons/si';
+import { BiMessageRoundedAdd } from 'react-icons/bi';
+
 function Header() {
   return (
     <section
       className="
-    m-auto grid h-14 max-w-7xl grid-cols-[1fr,2fr]
-    place-content-center p-2 py-10 lg:grid-cols-3
-    "
+      m-auto grid max-w-7xl grid-cols-2
+      place-content-center items-center p-2 pb-10 lg:grid-cols-3"
     >
       <div className="hidden lg:block"></div>
-      <div className="lg:justify-self-center">
-        <Link to="/" className="p-4 pl-0 lg:pl-4">
-          Logo
-        </Link>
-      </div>
-      <div className="justify-self-end ">
-        <Link
-          to="/suggest"
-          className="rounded-lg border-2 p-3 transition-all hover:border-black hover:bg-black hover:text-white"
-        >
-          Suggest Product
-        </Link>
-      </div>
+      <Link
+        to="/0"
+        className="group flex items-center gap-1 p-4 pl-0 transition-all lg:flex-col lg:gap-0 lg:justify-self-center lg:pl-4"
+      >
+        <SiStarship
+          className=" text-4xl transition-all duration-500 ease-in-out
+          group-hover:scale-110 lg:text-5xl"
+        />
+
+        <p className="font-medium">Prod-Rank</p>
+      </Link>
+      <Link
+        to="/suggest"
+        className="justify-self-end rounded-lg border-2 p-3 transition-all hover:border-black hover:bg-black hover:text-white"
+      >
+        <BiMessageRoundedAdd className="text-3xl sm:hidden" />
+        <p className="hidden sm:inline">Suggest Product</p>
+      </Link>
     </section>
   );
 }
