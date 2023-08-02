02a06ddc70783d9b6a49838e30934558a76d3252
Aleksey Marinkevich <alexeymarinkevich@gmail.com>
2023-08-02 16:56:54 +0900

    refactor: change named export to  default
    Changed regular function to an arrow function

--- src/components/pageLoader.tsx
+++ src/components/pageLoader.tsx
@@ -1,10 +1,12 @@
 import { TbLoader2 } from 'react-icons/tb';
 
-export function PageLoader() {
+const PageLoader = () => {
   return (
     <div className="flex h-[70vh] flex-col items-center justify-center gap-1">
       <TbLoader2 className="animate-spin text-6xl" />
       <p className="animate-pulse">Loading...</p>
     </div>
   );
-}
+};
+
+export default PageLoader;
