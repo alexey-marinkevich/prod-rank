38d2218ff3adb50b1c6419787c1d772305b62f32
Aleksey Marinkevich <alexeymarinkevich@gmail.com>
2023-10-18 08:30:14 +0900

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,37 @@
+import { DiGithubFull, DiGithubBadge } from 'react-icons/di';
+import { FaLinkedin, FaGithubSquare } from 'react-icons/fa';
+
+const Footer = () => (
+  <section className="p-2">
+    <footer className="m-auto flex max-w-7xl flex-col items-center rounded-3xl bg-black  p-10 text-white sm:flex-row sm:justify-between">
+      <a
+        className="flex"
+        href="https://github.com/alexey-marinkevich/prod-rank#welcome-to-prod-rank"
+      >
+        <DiGithubBadge className="mb-3 text-6xl sm:mb-0" />
+        <DiGithubFull className="hidden text-6xl sm:block" />
+      </a>
+      <section className="flex flex-col items-center sm:items-end">
+        <p className="font-thin">Demo by Alex Marinkevich</p>
+        <div className="flex text-3xl">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/alexey-marinkevich/"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/alexey-marinkevich/"
+          >
+            <FaGithubSquare />
+          </a>
+        </div>
+      </section>
+    </footer>
+  </section>
+);
+
+export default Footer;
