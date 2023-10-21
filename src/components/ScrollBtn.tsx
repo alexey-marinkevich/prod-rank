a32e839be9c716b2e5862866552293892bc76216
Aleksey Marinkevich <alexeymarinkevich@gmail.com>
2023-10-21 19:18:26 +0900

    style: add shadow to btn

--- src/components/ScrollBtn.tsx
+++ src/components/ScrollBtn.tsx
@@ -6,8 +6,8 @@ const ScrollBtn = () => (
     id="goTopBtn"
     onClick={scrollToTop}
     className="fixed bottom-5 right-5 rounded-lg border border-gray-300 bg-white/40 p-3 text-4xl text-gray-900
-    backdrop-blur-md transition-all duration-500 hover:border-gray-700 hover:bg-black/70 hover:text-gray-100
-    hover:shadow-md"
+    shadow-lg backdrop-blur-md transition-all duration-500 hover:border-gray-700 hover:bg-black/70
+    hover:text-gray-100"
   >
     <IoIosArrowRoundUp />
   </button>
