51b59c119317b7965a06fccf190912f5247742a8
Aleksey Marinkevich <alexeymarinkevich@gmail.com>
2023-10-26 00:25:37 +0900

    style: border radius top btn

--- src/components/ScrollBtn.tsx
+++ src/components/ScrollBtn.tsx
@@ -5,7 +5,7 @@ const ScrollBtn = () => (
   <button
     id="go-top-btn"
     onClick={scrollToTop}
-    className="fixed bottom-5 right-5 rounded-lg border border-gray-300/50 bg-white/40 p-3 text-4xl text-gray-900
+    className="fixed bottom-5 right-5 rounded-xl border border-gray-300/50 bg-white/40 p-3 text-4xl text-gray-900
     shadow-lg backdrop-blur-md transition-all duration-500 hover:border-gray-700 hover:bg-black/70
     hover:text-gray-100"
   >
