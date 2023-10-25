0f7ef43f18f75d28254c2a337787921d8ff4bb06
Aleksey Marinkevich <alexeymarinkevich@gmail.com>
2023-10-25 23:36:22 +0900

    refactor: nav bar appearance  duration

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ function NavBar() {
   return (
     <section
       id="nav-bar"
-      className="absolute z-50 m-auto w-full max-w-7xl p-2 transition-all duration-700 md:relative md:p-0"
+      className="absolute z-50 m-auto w-full max-w-7xl p-1 transition-all duration-1000 md:relative md:p-0"
     >
       <nav
         className="m-auto grid grid-cols-2 place-content-center items-center
