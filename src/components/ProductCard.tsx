7121a36e7a5fe2adeda18082f2a8c6b4e0332251
Aleksey Marinkevich <alexeymarinkevich@gmail.com>
2023-11-09 14:41:24 +0900

    fix: card text overflow

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -11,30 +11,23 @@ function ProductCard({ props }: { props: Product }) {
   return (
     <Link
       to={`/product/${id}`}
-      className="group flex h-[80vh] snap-start snap-always scroll-mt-3 flex-col overflow-hidden
-      rounded-3xl shadow-md transition-[transform,box-shadow] duration-500 md:h-[600px]
+      className="group flex h-[610px] snap-start snap-always scroll-mt-3 flex-col overflow-hidden
+      rounded-3xl shadow-md transition-[transform,box-shadow] duration-500
       md:snap-align-none md:snap-normal lg:h-[430px] lg:flex-row mouse:hover:scale-95
       mouse:hover:shadow-xl
       "
     >
-      <div
-        className="
-          h-3/4 w-full overflow-hidden rounded-3xl
-          lg:h-full lg:w-4/6
-        "
-      >
+      <div className="h-3/4 w-full overflow-hidden rounded-3xl lg:h-full lg:w-4/6">
         <div
           className={`h-full w-full bg-cover transition-transform duration-500 mouse:group-hover:scale-105`}
           style={{ backgroundImage: `url(${BASE_ROUTE + headImage})` }}
         />
       </div>
-      <div className="h-1/3 overflow-hidden p-3 pb-6 lg:h-auto lg:w-1/2 lg:p-6">
-        <h2 className="mb-2.5 font-serif text-2xl font-semibold uppercase">
+      <div className="m-4 h-1/3 overflow-hidden lg:m-6 lg:mb-8 lg:h-auto lg:w-1/2">
+        <h2 className="mb-2.5 font-serif text-2xl font-semibold uppercase leading-6">
           {productName}
         </h2>
-        <p className="h-4/5 overflow-hidden text-base font-light lg:h-[93%]">
-          {articleContent}
-        </p>
+        <p className="overflow-hidden text-base font-light">{articleContent}</p>
       </div>
     </Link>
   );
